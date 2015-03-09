ec8b3a69dcdeb3da016d604bcae39ecc2474eed2
defrummel <peterfrumau@gmail.com>
2015-03-09 16:03:00 +0100

    Drag and Dropping within the activityview

--- js/controller/mainController.js
+++ js/controller/mainController.js
@@ -4,6 +4,14 @@ var mainController = function(mainV, dayV, addActivityV){
 		dayV.addDay();
 	});
 	
-
+	function handleDragStart(e) {
+	  this.style.opacity = '0.4';  // this / e.target is the source node.
+	  alert("hoi");
+	}
+	
+	var cols = document.querySelectorAll('.activity');
+	[].forEach.call(cols, function(col) {
+	  col.addEventListener('dragstart', handleDragStart, false);
+	});
 
 }
