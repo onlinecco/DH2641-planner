040b83874b3dee05d6051d88b7a8938bb440f998
xmei9 <mxysnow@hotmail.com>
2015-03-09 02:22:41 +0100

    added modal for adding activity

--- js/view/addActivityView.js
+++ js/view/addActivityView.js
@@ -1,19 +1,5 @@
 var addActivityView = function(container){
 	
-	$("#addActivityForm").hide();
-	
-	this.addActivity = function(){
-    $("#addActivityForm").show();
-	}
-	
-	this.cancelActivity = function(){
-    $("#addActivityForm").hide();
-	}
-	
-	this.createActivity = function(){
-    $("#addActivityForm").hide();
-	}
-	
 	
 
 }
