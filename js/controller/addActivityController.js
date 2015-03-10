aca2088174612f1256dcf77d50965ce9ccb96944
robjochem <robjochem99@hotmail.com>
2015-03-10 17:51:42 +0100

    Modal name required
    When no name is entered in the modal a alert is shown and the modal
    won´t disappear.

--- js/controller/addActivityController.js
+++ js/controller/addActivityController.js
@@ -3,12 +3,16 @@ var addActivityController = function(view){
 	// Use onclick for now. Later object.addEventListener("click", myScript); See lecture slides: interaction
 	this.addActivityButton = function(){		//this.retrievedform = container.find("#activityForm");
 		//alert(this.retrievedform("fName").value);
-		
+		if (String($("#fName").val()) == ""){
+			alert("Fill in a name");
+		} else { 
+		$("#myModal").modal('hide');
 		var x = $("#fName").val();
 		var y = $("#fLength").val();
 		var z = $("#fType").val();
 		var q = $("#fDescription").val();
 		
 		model.addParkedActivity(new Activity(x,y,z,q));
+			}
 	}
 }
