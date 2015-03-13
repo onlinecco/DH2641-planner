71461cd277567327e5e7683fc853a71ed1aab27d
onlinecco <x-joy@live.cn>
2015-03-13 03:46:52 +0100

    Added graph to each day

--- js/controller/addActivityController.js
+++ js/controller/addActivityController.js
@@ -9,10 +9,15 @@ var addActivityController = function(view){
 		$("#myModal").modal('hide');
 		var x = $("#fName").val();
 		var y = $("#fLength").val();
+		if(isNaN(y)) alert("Please enter a number");
+		else{
+		if(y > 999) alert("Please fill in a time that is less than 1000");
+		else{
 		var z = $("#fType").val();
 		var q = $("#fDescription").val();
-		
 		model.addParkedActivity(new Activity(x,y,z,q));
+			
+			}}
 			}
 	}
 }
--- js/view/dayView.js
+++ js/view/dayView.js
@@ -26,13 +26,18 @@ var dayView = function(container){
 		for(day = 0; day< model.days.length; day++){
 			
 			//refill the day
-			
+			var totalTime = model.days[day].getTotalLength();
 			var time = model.days[day]._start;
 	
 			this.days[day].find(".startTime").html(model.days[day].getStart());
 			this.days[day].find(".endTime").html(model.days[day].getEnd());
-			this.days[day].find(".totalLength").html(model.days[day].getTotalLength());
+			this.days[day].find(".totalLength").html(totalTime);
 			
+			var preTime = 0;
+			var groTime = 0;
+			var disTime = 0;
+			var breTime = 0;
+
 			for(i = 0; i< model.days[day]._activities.length; i++){
 				var activity = $("<div>");
 	
@@ -44,15 +49,19 @@ var dayView = function(container){
 				switch (String(model.days[day]._activities[i].getType())){
 					case "Presentation":
 						activity.attr("class","row activity presentation");
+						preTime += model.days[day]._activities[i].getLength();
 						break;
 					case "Group Work":
 						activity.attr("class","row activity groupwork");
+						groTime += model.days[day]._activities[i].getLength();
 						break;
 					case "Discussion":
 						activity.attr("class","row activity discussion");
+						disTime += model.days[day]._activities[i].getLength();
 						break;
 					case "Break":
 						activity.attr("class","row activity break");
+						breTime += model.days[day]._activities[i].getLength();
 						break;
 					default:
 						activity.attr("class","row activity ");
@@ -61,7 +70,12 @@ var dayView = function(container){
 	
 				var start = time;
 				time = time + model.days[day]._activities[i].getLength();
-				start = Math.floor(start/60) + ":" + start % 60;
+				var hour = Math.floor(start/60);
+				if(hour < 10) hour = "0" + hour;
+				var min = start % 60;
+				if(min < 10) min = "0" + min;
+				//start = Math.floor(start/60) + ":" + start % 60;
+				start = hour + ":" + min;
 				var start_time = $("<div>"); 
 				start_time.attr("id","start_time"); 
 				start_time.attr("class","col-xs-3");
@@ -77,9 +91,40 @@ var dayView = function(container){
 				activity.append(name);
 	
 				this.days[day].find(".schedule").append(activity);
+
 				
 			}	
-			
+
+			var c=this.days[day].find(".graph");
+			c.empty();
+			var pre = $("<div>");
+			pre.attr("class","subGraph graphPre");
+			pre.height(preTime * c.height()/ totalTime);
+
+			var gro = $("<div>");
+			gro.attr("class","subGraph graphGro");
+			gro.height(groTime * c.height()/ totalTime);
+
+			var dis = $("<div>");
+			dis.attr("class","subGraph graphDis");
+			dis.height(disTime * c.height()/ totalTime);
+
+			var bre = $("<div>");
+			bre.attr("class","subGraph graphBre");
+			bre.height(breTime * c.height()/ totalTime);
+
+			var breBar = $("<div>");
+			breBar.attr("class","graphBre3");
+			breBar.height(0.7 * c.height());
+			breBar.width(c.width());
+
+			c.append(breBar);
+			c.append(pre);
+			c.append(gro);
+			c.append(dis);
+			c.append(bre);
+
+
 			this.days[day].find(".schedule").append("<div id='empty' class='row emptyactivity'> <div class='col-xs-3'> </div> <div class='col-xs-9 addemptyactivity glyphicon glyphicon-plus-sign'></div> </div>");
 
 		}
